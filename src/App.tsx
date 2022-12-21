d5f8d9fdb665c3a94f01ee1c65ae7c81343386c3
Sullivan <38718448+Epic-R-R@users.noreply.github.com>
2022-12-21 21:38:50 +0300

    Add App file

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,170 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Grid, styled, Paper, Container, Button } from "@mui/material";
+import React, { useState } from "react";
+import { GridOperationButton } from "./GridOperationButton";
+import { GridDigitButton } from "./GridDigitButton";
 
+const OutputContainer = styled("div")(({ theme }) => ({
+  width: "100%",
+  textAlign: "right",
+  height: "2em",
+  padding: theme.spacing(2),
+  fontSize: "3em",
+  overflow: "hidden",
+}));
+
+const CalculatorBase = styled(Paper)(({ theme }) => ({
+  padding: theme.spacing(2),
+  marginTop: theme.spacing(4),
+  borderRadius: 15,
+}));
 function App() {
+  const [currentValue, setCurrentValue] = useState("0");
+  const [operation, setOperation] = useState("");
+  const [prevValue, setPrevValue] = useState("");
+  const [Overwrite, setOverwrite] = useState(true);
+
+  const calculate = () => {
+    if (!prevValue || !prevValue) return currentValue;
+
+    const curr = parseFloat(currentValue);
+    const prev = parseFloat(prevValue);
+
+    let result;
+    switch (operation) {
+      case "÷":
+        result = prev / curr;
+        break;
+      case "*":
+        result = prev * curr;
+        break;
+      case "-":
+        result = prev - curr;
+        break;
+      case "+":
+        result = prev + curr;
+        break;
+    }
+    return result;
+  };
+
+  const equals = () => {
+    const val = calculate();
+    setCurrentValue(`${val}`);
+    setPrevValue("");
+    setOperation("");
+    setOverwrite(true);
+  };
+
+  const clear = () => {
+    setPrevValue("");
+    setOperation("");
+    setCurrentValue("0");
+    setOverwrite(true);
+  };
+
+  const del = () => {
+    setCurrentValue("0");
+    setOverwrite(true);
+  };
+
+  const percent = () => {
+    const curr = parseFloat(currentValue);
+    setCurrentValue((curr / 100).toString());
+  };
+
+  const selectOperation = (operation: string) => {
+    if (prevValue) {
+      const val = calculate();
+      setCurrentValue(`${val}`);
+      setPrevValue(`${val}`);
+    } else {
+      setPrevValue(currentValue);
+    }
+    setOperation(operation);
+    setOverwrite(true);
+  };
+  const setDigit = (digit: string) => {
+    if (currentValue[0] === "0" && digit === "0") return;
+    if (currentValue.includes(".") && digit == ".") return;
+    if (Overwrite && digit !== ".") {
+      setCurrentValue(digit);
+    } else {
+      setCurrentValue(`${currentValue}${digit}`);
+    }
+    setOverwrite(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="sm">
+      <CalculatorBase elevation={3}>
+        <Grid container spacing={1}>
+          <Grid item xs={12}>
+            <OutputContainer>{currentValue}</OutputContainer>
+          </Grid>
+          <Grid item container columnSpacing={1}>
+            <GridOperationButton
+              operation="AC"
+              selectOperation={clear}
+              selectedOperation={operation}
+            />
+            <GridOperationButton
+              operation="C"
+              selectOperation={del}
+              selectedOperation={operation}
+            />
+            <GridOperationButton
+              operation="%"
+              selectOperation={percent}
+              selectedOperation={operation}
+            />
+            <GridOperationButton
+              operation="÷"
+              selectOperation={selectOperation}
+              selectedOperation={operation}
+            />
+          </Grid>
+          <Grid item container columnSpacing={1}>
+            <GridDigitButton digit="7" enterDigit={setDigit} />
+            <GridDigitButton digit="8" enterDigit={setDigit} />
+            <GridDigitButton digit="9" enterDigit={setDigit} />
+            <GridOperationButton
+              operation="*"
+              selectOperation={selectOperation}
+              selectedOperation={operation}
+            />
+          </Grid>
+          <Grid item container columnSpacing={1}>
+            <GridDigitButton digit="4" enterDigit={setDigit} />
+            <GridDigitButton digit="5" enterDigit={setDigit} />
+            <GridDigitButton digit="6" enterDigit={setDigit} />
+            <GridOperationButton
+              operation="-"
+              selectOperation={selectOperation}
+              selectedOperation={operation}
+            />
+          </Grid>
+          <Grid item container columnSpacing={1}>
+            <GridDigitButton digit="1" enterDigit={setDigit} />
+            <GridDigitButton digit="2" enterDigit={setDigit} />
+            <GridDigitButton digit="3" enterDigit={setDigit} />
+            <GridOperationButton
+              operation="+"
+              selectOperation={selectOperation}
+              selectedOperation={operation}
+            />
+          </Grid>
+          <Grid item container columnSpacing={1}>
+            <GridDigitButton digit="0" enterDigit={setDigit} xs={6} />
+            <GridDigitButton digit="." enterDigit={setDigit} />
+            <Grid item xs={3}>
+              <Button fullWidth variant="contained" onClick={equals}>
+                =
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
+      </CalculatorBase>
+    </Container>
   );
 }
 
